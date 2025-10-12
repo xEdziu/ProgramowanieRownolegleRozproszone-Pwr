f2cecc910c41e0929028592aa8357fecdc32e685
Paulina Szulc <paulinaszulc23@interia.pl>
2025-10-12 14:07:40 +0200

    Poprawa nazewnictwa w pierwszych dwoch zadaniach

--- Lista1/Program.cs
+++ Lista1/Program.cs
@@ -4,17 +4,17 @@
     {
         const int NumberOfMiners = 5;
         static int Ore1 = 2000;
-        static int Magazine = 0;
+        static int Warehouse = 0;
         const int VehicleCapacity = 200;
         const int TimeAquiringOneUnit = 10; //ms
         const int TimeUnloadingOneUnit = 10; //ms
         const int TimeToTravelToDestination = 10000; //ms => 10s
         static SemaphoreSlim mine = new SemaphoreSlim(2, 2);
-        static SemaphoreSlim magazine = new SemaphoreSlim(1, 1);
+        static SemaphoreSlim warehouse = new SemaphoreSlim(1, 1);
         static object lockMine = new object();
-        static object lockMagazine = new object();
+        static object lockWarehouse = new object();
         static int minerId = 0;
-        static readonly object consoleLock = new object(); 
+        static readonly object consoleLock = new object();
         static readonly string[] minerStatus = new string[NumberOfMiners];
 
 
@@ -41,12 +41,12 @@
             try { displayTask.Wait(); } catch { }
             Console.WriteLine("Resources left in Ore 1: " + Ore1);
             Console.WriteLine("All mining completed.");
-            Console.WriteLine("Total time taken: " + (endTime - startTime)/1000 + "s");
+            Console.WriteLine("Total time taken: " + (endTime - startTime) / 1000 + "s");
             lock (consoleLock)
             {
                 Console.SetCursorPosition(0, 7 + NumberOfMiners);
                 Console.WriteLine($"Final Ore: {Ore1}".PadRight(60));
-                Console.WriteLine($"Final Magazine: {Magazine}".PadRight(60));
+                Console.WriteLine($"Final Warehouse: {Warehouse}".PadRight(60));
                 Console.CursorVisible = true;
             }
         }
@@ -71,23 +71,23 @@
                     }
                 }
                 mine.Release();
-                // Traveling to magazine
-                minerStatus[idx] = "Traveling to magazine";
+                // Traveling to warehouse
+                minerStatus[idx] = "Traveling to warehouse";
                 Thread.Sleep(TimeToTravelToDestination);
                 // Unloading
-                minerStatus[idx] = "Waiting for magazine access";
-                magazine.Wait();
+                minerStatus[idx] = "Waiting for warehouse access";
+                warehouse.Wait();
                 minerStatus[idx] = "Unloading";
-                lock (lockMagazine)
+                lock (lockWarehouse)
                 {
                     for (int i = 0; i < amountToMine; i++)
                     {
                         Thread.Sleep(TimeUnloadingOneUnit);
-                        Magazine++;
+                        Warehouse++;
                     }
                     minerStatus[idx] = "Unloaded";
                 }
-                magazine.Release();
+                warehouse.Release();
                 // Don't continue if no ore left
                 if (Ore1 <= 0)
                 {
@@ -113,13 +113,13 @@
                     Console.WriteLine($"Ore remaining: {Volatile.Read(ref Ore1)} units".PadRight(60));
 
                     Console.SetCursorPosition(0, 2);
-                    Console.WriteLine($"Magazine: {Volatile.Read(ref Magazine)} units".PadRight(60));
+                    Console.WriteLine($"Warehouse: {Volatile.Read(ref Warehouse)} units".PadRight(60));
 
                     Console.SetCursorPosition(0, 3);
                     Console.WriteLine($"Mine semaphore free slots: {mine.CurrentCount}".PadRight(60));
 
                     Console.SetCursorPosition(0, 4);
-                    Console.WriteLine($"Magazine semaphore free slots: {magazine.CurrentCount}".PadRight(60));
+                    Console.WriteLine($"Warehouse semaphore free slots: {warehouse.CurrentCount}".PadRight(60));
 
                     Console.SetCursorPosition(0, 5);
                     Console.WriteLine(new string('-', 60));
